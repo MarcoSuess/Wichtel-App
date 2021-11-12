b73e9ddbb1683889f1f6c888538170d4d73d0165
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-11-12 22:18:23 +0100

    remove snowflakes animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,12 @@ import { UserService } from './services/user/user.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'wichtel-app';
-
-  snowFlakeImg = [
-    'assets/icons/50-Christmas-Icons-24.png',
-    'assets/icons/50-Christmas-Icons-21.png',
-  ];
-
-  allSnowFlakes: any = [];
   constructor(
     public userService: UserService,
     public router: Router,
     private location: Location,
     public channelService: ChannelService
-  ) {
-    for (let i = 0; i < 3; i++) {
-      let randomIndex = Math.floor(Math.random() * this.snowFlakeImg.length);
-      this.allSnowFlakes.push(this.snowFlakeImg[randomIndex]);
-    }
-  }
+  ) {}
 
   checkRoute() {
     return this.router.url.includes('dashboard');
@@ -35,12 +22,5 @@ export class AppComponent {
 
   goBack() {
     this.location.back();
-  }
-
-  setMyStyles(index: number) {
-    let styles = {
-      right: 40 * index + '%',
-    };
-    return styles;
   }
 }
