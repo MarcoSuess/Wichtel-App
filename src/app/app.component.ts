3aac6a93ffe613c5852795d37bb757004fa61bfa
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-11-12 22:03:23 +0100

    snowflakes animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,31 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { ChannelService } from './services/channel/channel.service';
+import { UserService } from './services/user/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'wichtel-app';
-  
-  constructor( public router: Router,private location: Location, public channelService: ChannelService) {
 
+  snowFlakeImg = [
+    'assets/icons/50-Christmas-Icons-24.png',
+    'assets/icons/50-Christmas-Icons-21.png',
+  ];
+
+  allSnowFlakes: any = [];
+  constructor(
+    public userService: UserService,
+    public router: Router,
+    private location: Location,
+    public channelService: ChannelService
+  ) {
+    for (let i = 0; i < 3; i++) {
+      let randomIndex = Math.floor(Math.random() * this.snowFlakeImg.length);
+      this.allSnowFlakes.push(this.snowFlakeImg[randomIndex]);
+    }
   }
 
   checkRoute() {
@@ -20,5 +35,12 @@ export class AppComponent {
 
   goBack() {
     this.location.back();
+  }
+
+  setMyStyles(index: number) {
+    let styles = {
+      right: 40 * index + '%',
+    };
+    return styles;
   }
 }
